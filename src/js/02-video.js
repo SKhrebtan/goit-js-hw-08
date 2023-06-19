5be0318401e78e9618e30f0a8a08f6fc17fd06a0
SKhrebtan <shrebtan@gmail.com>
2023-06-19 23:18:24 +0300

    2 - vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage'
+
+const TIMECODE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);  
+
+player.setCurrentTime(load(TIMECODE_KEY));
+ 
+const onTimeUpdateToStorage = function (data) {
+    console.log( JSON.stringify(data.seconds))
+    save(TIMECODE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(onTimeUpdateToStorage, 1000));
 
